9f3ad29a47b714535e940aa2477c1afdf40c053d
maar-ten <github@laurs.nl>
2014-03-24 20:20:07 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Quick Image Search
+===
+---
+
+*initiate a secure search on Google Images from the context menu of your Chrome web browser*
+
+Usage
+---
+After installing this Chrome browser extension there'll be a new entry in your context menu (right-click menu) when an
+image or some selected text was right-clicked. Selecting this menu option will initiate a Google Image search via HTTPS.
+
+Permissions
+---
+This browser extension doesn't require any permissions
+
+Legal information
+---
+The icon was designed by Yellowicon.com (taken from findicons.com, which said it was GNU/GPL licensed).
+
+The code for this extension was largely copied and modified from Cheng Lee's MapMe extension. I've left his copyright
+statement in the source files.
+
---
+++ eventPage.js
@@ -0,0 +1,42 @@
+/**
+ * Copied and modified from Cheng Lee's MapMe extension. Here is his statement:
+ *
+ * Copyright (c) 2012 Cheng Lee. All rights reserved.  Use of this
+ * source code is governed by a BSD-style license that can be found in the
+ * LICENSE file.
+ *
+ * Icon designed by Yellowicon.com (taken from findicons.com, which said it was GNU/GPL licensed)
+ */
+
+/**
+ * Returns a handler which will open a new window when activated.
+ */
+function getClickHandler() {
+    return function (info, tab) {
+        var selected, url = "https://encrypted.google.com/";
+        if (info.selectionText) {
+            selected = info.selectionText;
+
+            // Join multiple lines into one separated with comma
+            selected = selected.replace(/\n/g, ', ');
+            url += 'images?q=' + selected;
+        }
+
+        if (info.mediaType == "image") {
+            url += 'searchbyimage?image_url=' + info.srcUrl;
+        }
+
+        // Create a new tab to the info page.
+        chrome.tabs.create({url: url});
+    };
+}
+
+/**
+ * Create a context menu which will only show up for images.
+ */
+chrome.contextMenus.create({
+    "title":    chrome.i18n.getMessage("search_in_google_images"),
+    "type":     "normal",
+    "contexts": ["selection", "image"],
+    "onclick":  getClickHandler()
+});
